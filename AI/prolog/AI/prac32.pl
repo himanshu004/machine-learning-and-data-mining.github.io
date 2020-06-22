3881591a935a2c0c860c2b5b5acac2c65495ec8b
Himanshu Madan <himanshumadan1059@gmail.com>
2020-06-23 00:35:43 +0530

    knn code added

--- AI/prolog/AI/prac32.pl
+++
@@ -1,7 +0,0 @@
-conc([], L2, L2).
-conc([X|Y], L2, [X|Z]):- conc(Y, L2, Z).
-
-my_permutation([], []).
-my_permutation(L, [H|T]):- conc(V, [H|U], L),
-                            conc(V, U, W),
-                            my_permutation(W, T).
--- AI/prolog/AI/prac9.pl
+++
@@ -1,7 +0,0 @@
-edge(a,b).
-edge(b,c).
-edge(c,d).
-edge(d,e).
-edge(e,f).
-path(X,Y):- edge(X,Y),!.
-path(X,Y):- edge(X,Z),path(Z,Y).
